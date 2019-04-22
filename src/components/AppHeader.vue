cd729d36a0fe682cddd5693d0bee682a1a1bea13
pavel-else <de.pavel.se@yandex.ru>
2019-04-22 06:56:10 +0300

    AppHeader - added icon-side-toolbar - maked responsive

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,22 @@
 <template>
     <v-app>
+        <v-navigation-drawer absolute temporary v-model="drawer" class="hidden-md-and-up">
+            <v-list>
+                <v-list-tile v-for="(item, index) in menuItems" :key="index">
+                    <v-list-tile-action>
+                        <v-icon v-html="item.icon" left flat></v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
         <v-toolbar class="primary" app dark>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
             <v-toolbar-title v-text="'Ich lerne Deutsch'"></v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-toolbar-items>
-                <!-- <v-btn v-for="(item, i) in menuItems" :key="i" flat :to="item.route">
-                    <v-icon left v-html="item.icon"></v-icon>
-                    {{ item.title }}
-                </v-btn> -->
+            <v-toolbar-items class="hidden-sm-and-down">
                 <v-btn flat v-for="(item, index) in menuItems" :key="index" :to="item.route">
                     <v-icon left v-html="item.icon"></v-icon>
                     {{ item.title }}
@@ -19,6 +28,11 @@
 
 <script>
 export default {
+    data() {
+        return {
+            drawer: false,
+        }
+    },
     computed: {
         menuItems() {
             return [
