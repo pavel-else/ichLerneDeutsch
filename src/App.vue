2bc800994540392612d56d9361175b40f0db4eac
pavel-else <de.pavel.se@yandex.ru>
2019-04-22 08:00:06 +0300

    App - creaate and included footer component

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,17 @@
     <v-content>
       <router-view/>
     </v-content>
+    <app-footer></app-footer>
   </v-app>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader'
+import AppFooter from '@/components/AppFooter'
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
   }
   
 }
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <v-footer :fixed="true" app>
+            <span>Ich Lerne Deutsch &copy;2018</span>
+        </v-footer>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
