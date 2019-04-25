3862b9a057aa9b58614940a558ef3e19bed03774
pavel-else <de.pavel.se@yandex.ru>
2019-04-25 15:56:23 +0300

    App - added firebase (dont installed)

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyA2TKg1Bq2ugA7XbcjrzELddX5ylVnz3AU",
+    authDomain: "ichlearnedeutsch.firebaseapp.com",
+    databaseURL: "https://ichlearnedeutsch.firebaseio.com",
+    projectId: "ichlearnedeutsch",
+    storageBucket: "ichlearnedeutsch.appspot.com",
+    messagingSenderId: "870696939197"
+}
